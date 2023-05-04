ce31bb7874a0f5c0f13559ed7f04abdfa04b457d
basitmanzooriQuasar <basit.manzoor@iquasar.com>
2023-05-04 17:56:56 +0530

    day2

---
+++ complete-javascript-course-master/complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,153 @@
+// let js = "amazing";
+// if (js === "amazing") alert("JavaScript is FUN");
+// console.log(40 + 10 + 90 - 2);
+
+// let firstName = "Basit";
+// let lastName = "Manzoor";
+
+// console.log(firstName);
+// console.log(lastName);
+
+// let PI = 3.1415;
+// console.log(PI);
+
+// let country = "India";
+// let continent = "Asia";
+// let population = 123450;
+
+// console.log(country);
+// console.log(continent);
+// console.log(population);
+
+// let checker = true; //boolean
+// console.log(typeof checker); //will tell you the type of data type used
+
+// console.log(typeof 23);
+// console.log(typeof "Basit");
+
+// checker = 23;
+// console.log(checker);
+
+// //undefined
+// let undefinedCheck;
+
+// console.log(typeof undefinedCheck);
+
+// undefinedCheck = "basit";
+// console.log(typeof null);
+
+// //assignment data types
+// let isIsland = false;
+// let language;
+
+// console.log(typeof isIsland);
+// console.log(typeof language);
+
+// let age = 30;
+// age = 31; //mutating the variable, we use let
+
+// //value in a const varaible cannot change
+// const birthYear = 1998;
+// // birthYear = 1999; not possible
+
+// //var keyword to declare variables
+// var job = "SDE";
+// console.log(job);
+
+// job = "SDE1";
+// console.log(job);
+
+//Basic Operators
+// const presentYear = 2023;
+// const age = presentYear - 1998;
+// const ageSarah = presentYear - 1992;
+
+// const diiv = 29 / 3;
+// const mull = 3 * 3;
+// const expo = 2 ** 3;
+
+// console.log(presentYear, diiv, mull, expo);
+
+// console.log(age, ageSarah);
+
+// const firstName = "basit";
+// const lastName = "manzoor";
+
+// console.log(firstName + " " + lastName);
+
+// console.log(age === ageSarah);
+
+// //coding challenge 1
+// let markHeight = 6;
+// let markWeight = 79;
+
+// let johnHeight = 5.7;
+// let johnWeight = 83;
+
+// const BMI = markWeight / markHeight ** 2;
+// const BMI2 = johnWeight / johnHeight ** 2;
+
+// let markHeigherBMI = BMI > BMI2;
+
+// // if (BMI > BMI2) {
+// //   markHeigherBMI = true;
+// // } else {
+// //   markHeigherBMI = false;
+// // }
+
+// console.log(BMI, BMI2, markHeigherBMI);
+
+// //Strings and template literals
+// const firstName = "Jonas";
+// const job = "teacher";
+// const birthYear = 1998;
+// const year = 2039;
+
+// const jonas =
+//   "I am " +
+//   firstName +
+//   ", a " +
+//   (year - birthYear) +
+//   " years old " +
+//   job +
+//   " !";
+
+// console.log(jonas);
+
+// console.log(`this is a template literal string..`);
+// console.log(`this is first line
+// this is second line
+// this is third line with some symbols in next line
+// <> * $`);
+
+// console.log((2 * 1) / 3 / 1);
+
+// const age = 18;
+
+// if (age > 18) {
+//   console.log(`first condition`);
+// } else if (age === 18) {
+//   console.log(`second condition`);
+// } else {
+//   console.log(`third condition`);
+// }
+
+//coding challenge 2
+// let markHeight = 6;
+// let markWeight = 79;
+
+// let johnHeight = 5.7;
+// let johnWeight = 83;
+
+// const BMI = markWeight / markHeight ** 2;
+// const BMI2 = johnWeight / johnHeight ** 2;
+
+// let markHeigherBMI;
+
+// if (BMI > BMI2) {
+//   markHeigherBMI = true;
+// } else {
+//   markHeigherBMI = false;
+// }
+
+// console.log(BMI, BMI2, markHeigherBMI);
