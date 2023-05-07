d771870902583a1e21365011911b67107280960b
basitmanzooriQuasar <basit.manzoor@iquasar.com>
2023-05-07 11:18:46 +0530

    section 3 start

---
+++ complete-javascript-course-master/complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,58 @@
+"use strict";
+
+// let hasDriversLicense = false;
+// const passTest = true;
+
+// if (passTest) hasDriversLicense = true;
+// if (hasDriversLicense) console.log(`i can drive`);
+
+// const interface = "Audio"; //reserves interface keyword
+
+// const private = "salary"; //unexpected strict mode reserved word
+
+//functions
+// a piece of code that can be used multiple times
+
+// function helper() {
+//   console.log("My name is Basit");
+// }
+
+// //calling/running/invoking a function
+// helper();
+// helper();
+// helper();
+
+// function fruitProcessor(apples, oranges) {
+//   //contains parameters
+//   //console.log(apples, oranges);
+//   const juice = `Juice with ${apples} apples and ${oranges} oranges`;
+//   return juice;
+// }
+
+// //calling foodprocessor
+
+// // fruitProcessor(5, 0); //arguments
+
+// const appleJuice = fruitProcessor(5, 0);
+// console.log(appleJuice);
+
+//function for calculating age
+// function calcAge1(birthYear) {
+//   const age = 2037 - birthYear;
+//   return age;
+// }
+
+// const ans1 = calcAge1(1998); //calling function having arguments int it
+
+// console.log(ans1);
+
+// //anonymous function
+// const calcAge2 = function (birthYear) {
+//   return 2037 - birthYear;
+// };
+
+// const ans2 = calcAge2(1991);
+
+// console.log(ans2);
+
+//Arrow Function
