89fefb408bdb8a45eca71254aaac3d25630ef790
basitmanzooriQuasar <basit.manzoor@iquasar.com>
2023-05-07 12:37:33 +0530

    7thmay2

--- complete-javascript-course-master/complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ complete-javascript-course-master/complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -94,3 +94,50 @@
 
 // const ans = fruitProcessor(2, 3);
 // console.log(ans);
+
+//Coding challenge 1
+
+// const calAvg = (score1, score2, score3) => {
+//   return (score1, score2, score3) / 3;
+// };
+
+// const dolphinsAvg = calAvg(44, 23, 71);
+// const koalasAvg = calAvg(65, 54, 49);
+
+// function checkWinner(dolphinsAvg, koalasAvg) {
+//   if (dolphinsAvg > koalasAvg) {
+//     return `Dolphins win`;
+//   } else {
+//     return `Koalas win`;
+//   }
+// }
+
+// const ans = checkWinner(dolphinsAvg, koalasAvg);
+// console.log(ans);
+
+// //Arrays Data Structure
+// const friends = ["Basit", "Danish", "Abbas", "Annus"];
+
+// console.log(friends);
+// console.log(friends[0]); //with 0 based indexing, it will print first element of array
+
+// //other way to make an array
+// const age = new Array(19, 29, 23, 41);
+
+// console.log(age[2]); //printing third element of array age
+// console.log(age.length); //in order to calculate length or size of the array
+
+// console.log(age[age.length - 1]); //in order to access last element of the array
+// friends[2] = "Drake"; //changing the elements of the array
+// console.log(friends);
+// //Array is not a primitive data type, so we can mutate array even though it was declared as const. but we cant change the entire array
+
+// const basit = ["Basit", "Manzoor", 2023 - 1998, friends];
+// console.log(basit);
+
+// function calcAge(birthYear) {
+//   return 2023 - birthYear;
+// }
+
+// const years = [1990, 1991, 1992, 1993];
+// console.log(calcAge(years[3]));
