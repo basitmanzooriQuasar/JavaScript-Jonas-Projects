a9e19da524127c0723aeba0c5b9b77fc0cb9bb70
basitmanzooriQuasar <basit.manzoor@iquasar.com>
2023-05-09 18:47:55 +0530

    Modal project done

--- complete-javascript-course-master/complete-javascript-course-master/06-Modal/starter/script.js
+++ complete-javascript-course-master/complete-javascript-course-master/06-Modal/starter/script.js
@@ -1,1 +1,37 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+
+const overlay = document.querySelector('.overlay');
+
+const btnCloseModal = document.querySelector('.close-modal');
+
+const btnsOpenModel = document.querySelectorAll('.show-modal');
+
+const openModel = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+  //overlay.style.display = 'block';
+};
+
+for (let i = 0; i < btnsOpenModel.length; i++) {
+  btnsOpenModel[i].addEventListener('click', openModel);
+}
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+btnCloseModal.addEventListener('click', closeModal);
+//if we write function as closeModal(), it would immediately call the function, we do not want that
+overlay.addEventListener('click', closeModal);
+
+//closing modal while pressing the Esc key
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape') {
+    if (!modal.classList.contains('hidden')) {
+      closeModal();
+    }
+  }
+});
