55c10aaf12c319fc750b0cbfdcf29d26699f0183
basitmanzooriQuasar <basit.manzoor@iquasar.com>
2023-05-29 08:33:29 +0530

    DOM

--- complete-javascript-course-master/complete-javascript-course-master/13-Advanced-DOM-Bankist/starter/script.js
+++ complete-javascript-course-master/complete-javascript-course-master/13-Advanced-DOM-Bankist/starter/script.js
@@ -3,29 +3,74 @@
 ///////////////////////////////////////
 // Modal window
 
+//Selecting Modal using DOM query Selector
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 
+//function where we remove our hidden class from CSS
 const openModal = function () {
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
 
+//function where we add the hidden class in CSS
 const closeModal = function () {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
 };
 
-for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', openModal);
-
+//opening modal on button click for all buttons
+btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
+//closing the modal while clicking on cross button
 btnCloseModal.addEventListener('click', closeModal);
+//closing the modal while clicking on overlay
 overlay.addEventListener('click', closeModal);
 
+//closing modal while pressing Escape key.
 document.addEventListener('keydown', function (e) {
   if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
     closeModal();
   }
 });
+//selecting
+
+const header = document.querySelector('.header');
+//creating a cookie message
+const message = document.createElement('div');
+
+message.classList.add('cookie-message'); //adding class css
+message.innerHTML =
+  'This is cookies to keep track of your preferences <button class="btn btn-cookie">Got it!</button>';
+
+//inserting
+header.append(message);
+
+//deleting
+document.querySelector('.btn-cookie').addEventListener('click', function () {
+  message.remove();
+});
+
+//styles
+message.style.backgroundColor = '#37383d';
+message.style.width = '100%';
+
+//smooth scrolling
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+const sectionOp = document.querySelector('#section--2');
+btnScrollTo.addEventListener('click', function (e) {
+  // const s1Cords = section1.getBoundingClientRect();
+  sectionOp.scrollIntoView({ behavior: 'smooth' });
+});
+
+//adding and deleting Event listener after it is hovered
+const h1 = document.querySelector('h1');
+
+const h1alert = function (e) {
+  alert('This is an EventListener and you hovered on heading 1');
+
+  h1.removeEventListener('mouseover', h1alert);
+};
+h1.addEventListener('mouseover', h1alert);
